6372e6147416f832c4a381bb1e173a278397857a
Mohammed <mt2001@hotmail.com>
2017-10-14 11:37:55 +0300

    First commit

---
+++ Virtual_Toutrists/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  Virtual_Toutrists
+//
+//  Created by محمد عايض العتيبي on 1/23/1439 AH.
+//  Copyright © 1439 code schoole. All rights reserved.
+//
+
+import Foundation
---
+++ Virtual_Toutrists/Virtual_Toutrists/PhotoAlbumController.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoAlbumController.swift
+//  Virtual_Toutrists
+//
+//  Created by محمد عايض العتيبي on 1/24/1439 AH.
+//  Copyright © 1439 code schoole. All rights reserved.
+//
+
+import Foundation
