c35c6fcddcf960956ece78192b48a55b1a8ff6bc
Mohammed <mt2001@hotmail.com>
2017-10-31 20:24:02 +0300

    new methods

---
+++ Virtual_Toutrists/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Virtual_Toutrists
+//
+//  Created by محمد عايض العتيبي on 1/24/1439 AH.
+//  Copyright © 1439 code schoole. All rights reserved.
+//
+
+import Foundation
---
+++ Virtual_Toutrists/Virtual_Toutrists/mapExtenstion.swift
@@ -0,0 +1,9 @@
+//
+//  mapExtenstion.swift
+//  Virtual_Toutrists
+//
+//  Created by محمد عايض العتيبي on 2/1/1439 AH.
+//  Copyright © 1439 code schoole. All rights reserved.
+//
+
+import Foundation
